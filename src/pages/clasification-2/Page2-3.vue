8b739388dc48be96fcc9cc16dca23d0ba0b068c1
rboockv <rudyboockv@gmail.com>
2024-03-18 21:12:36 -0300

    added anchors divs

--- src/pages/clasification-2/Page2-3.vue
+++ src/pages/clasification-2/Page2-3.vue
@@ -1,11 +1,15 @@
 <template>
   <q-page class="flex column q-ml-xl">
+    <div id="anchor-tema1" class="anchorDiv"></div>
     <h2>Clasificación 2.3 - tema 1</h2>
     <p>descripcion de ejemplo</p>
+    <div id="anchor-tema2" class="anchorDiv"></div>
     <h2>Clasificación 2.3 - tema 2</h2>
     <p>descripcion de ejemplo</p>
+    <div id="anchor-tema3" class="anchorDiv"></div>
     <h2>Clasificación 2.3 - tema 3</h2>
     <p>descripcion de ejemplo</p>
+    <div id="anchor-tema4" class="anchorDiv"></div>
     <h2>Clasificación 2.4 - tema 4</h2>
     <p>descripcion de ejemplo</p>
   </q-page>
@@ -16,3 +20,8 @@ defineOptions({
   name: 'page-2-3'
 });
 </script>
+<style>
+.anchorDiv {
+  visibility: hidden;
+}
+</style>
